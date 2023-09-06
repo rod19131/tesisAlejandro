644904ebfb7c8105baafb6d4370a0b3ef62ed63b
José Alejandro Rodríguez Porras <rod19131@uvg.edu.gt>
2023-09-05 18:12:21 -0600

    angle compensation extraction
    Se agregó un código de procesamiento de datos para calibración.

---
+++ codigo/comunicacion_pololu/robotat_3pi_python_v2/procesamiento_datos_calibracion.py
@@ -0,0 +1,29 @@
+import pickle
+from funciones_conjunto import *
+#teoria 2
+#lista = [1,2]
+#lista2 = [lista]
+#print(lista)
+#print(lista2)
+# Load variables from the file
+with open('markers_alineados.pickle', 'rb') as file:
+    poses_markers = pickle.load(file)
+    #pose = pickle.load(file)
+
+#eul1 = quaternion_to_euler(list(pose_por_agente[0]),'xyz')
+markers_eul = quat2eul(poses_markers,'zyx') 
+
+#print(pose_por_agente)
+print("conversion")
+i = 0
+i_agente = 0
+for marker in markers_eul:
+    i_agente = i_agente+1
+    if (i_agente == 15):
+        i_agente = 16
+
+    print(f"agent: {i_agente}\n angle compensation: {markers_eul[i,3]}°")
+    i = i+1
+#print(eul1)
+#print(markers_eul)
+#print(pose)
